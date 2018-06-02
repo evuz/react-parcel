661e8c021197979b193327c9a160933c6eed614d
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-06-03 01:16:28 +0200

    Improve Header component

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -3,20 +3,26 @@ import PropTypes from 'prop-types';
 
 export const Header = styled.div`
   width: 100%;
-  position: fixed;
+  position: ${({ fixed }) => (fixed ? 'fixed' : 'absolute')};
   opacity: 1;
   top: 0;
   left: 0;
   height: ${({ height }) => height};
   background-color: ${({ backgroundColor }) => backgroundColor};
+  padding: 8px 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 Header.propTypes = {
   height: PropTypes.string,
   backgroundColor: PropTypes.string,
+  fixed: PropTypes.bool,
 };
 
 Header.defaultProps = {
   height: '56px',
   backgroundColor: 'lightgray',
+  fixed: false,
 };
