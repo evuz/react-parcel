2a1be27afee60a983eb83635720708087347e1d1
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-06-03 23:20:16 +0200

    App show news components

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,13 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components';
 
 import { Header } from './components/Header';
 import { Container } from './components/Container';
-import { Tabs } from './components/Tabs';
+import { Icon } from './components/Icon';
+import { Menu } from './components/Menu';
+import { Button } from './components/Button';
 
-export const App = () => (
-  <Fragment>
-    <Header />
-    <Container marginBottom="56px">My container</Container>
-    <Tabs />
-  </Fragment>
-);
+const Text = styled.p`
+  font-family: sans-serif;
+  font-size: 24px;
+  text-align: center;
+`;
+
+export class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Menu
+          show
+          open={open => {
+            this.openMenu = open;
+          }}
+        >
+          <Text>Menu</Text>
+        </Menu>
+        <Header fixed>
+          <Button
+            onClick={() => {
+              this.openMenu();
+            }}
+          >
+            <Icon icon="bars" size="22px" />
+          </Button>
+        </Header>
+        <Container />
+      </Fragment>
+    );
+  }
+}
